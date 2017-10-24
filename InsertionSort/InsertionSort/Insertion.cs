13f02d23ac59bf36c4326814c4e28eab7c38e3a0
Gregory Dukes <kcils360@gmail.com>
2017-10-23 17:34:37 -0700

    initial build, tried making the sort as a separate class

---
+++ InsertionSort/InsertionSort/Insertion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertionSort
+{
+    class Insertion
+    {
+        public void InsertionSort(int[] arr)
+        {
+            for(int i = 1; i > arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+            //print to console
+            foreach( var point in arr )
+            {
+                Console.WriteLine(point);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] monday = { 10, 25, 14, 9, 32, 7 };
+
+            Insertion sort = new Insertion();
+            sort.InsertionSort(monday);
+        }
+    }
+}
