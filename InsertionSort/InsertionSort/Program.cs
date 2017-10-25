d1d85eb8ca74bc59dc14d5c39c77ba9b3b44b1c9
Gregory Dukes <kcils360@gmail.com>
2017-10-24 17:35:07 -0700

    completed merge sort

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -7,14 +7,20 @@ namespace InsertionSort
         static void Main(string[] args)
         {
             int[] arr1 = { 10, 25, 14, 9, 32, 7 };
+            int len1 = arr1.Length-1;
+
             int[] arr2 = { 69, 56, 77, 44, 8, 0, 566, 111 };
+            int len2 = arr2.Length-1;
+            //Insertion sort = new Insertion();
 
-            Insertion sort = new Insertion();
+            //sort.InsertionSort(arr1);
+            //sort.InsertionSort(arr2);
 
-            sort.InsertionSort(arr1);
-            sort.InsertionSort(arr2);
+            Merge merger = new Merge();
 
 
+            merger.SortMethod(arr2, 0, len2);
+            merger.Print(arr2);
 
         }
     }
